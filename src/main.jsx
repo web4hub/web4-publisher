56ee22df90b1a23f204dfb4245ad741a38f6fe01
web4 <lunghugh@gmail.com>
2026-09-18 03:17:36 +0100

    Improve app entry point safety and simplify rendering

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,12 @@ import App from "./App.jsx";
 import "./styles/base.scss";
 import "./styles/themes.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    "Web4 Publisher could not find the #root element. Ensure index.html contains <div id='root'></div>."
+  );
+}
+
+ReactDOM.createRoot(rootElement).render(<App />);
