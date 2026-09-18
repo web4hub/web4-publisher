bced350777a27744ecbaf3409a6b3f1335cfc729
web4hub <lunghugh@gmail.com>
2026-09-18 01:12:02 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,43 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
+  resolve: {
+    alias: {
+      '@styles': resolve(__dirname, 'src/styles'),
+      '@posts': resolve(__dirname, 'posts'),
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+
   build: {
-    outDir: "dist",
-    emptyOutDir: true
-  }
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'theme-default': resolve(
+          __dirname,
+          'src/styles/reset.scss'
+        ),
+        'theme-win95': resolve(
+          __dirname,
+          'src/styles/win95.scss'
+        ),
+        'flavour-glitch': resolve(
+          __dirname,
+          'src/styles/flavours/glitch/reset.scss'
+        ),
+      },
+    },
+  },
 });
